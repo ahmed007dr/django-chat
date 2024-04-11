fa8a7a8edb98d38abf8c7862d7ce00943386b8be
ahmed007dr <ahmed007eg@gmail.com>
2024-04-11 16:29:10 +0200

    log in template

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/',auth_views.LoginView.as_view() , name='logout'),
 
 ]
