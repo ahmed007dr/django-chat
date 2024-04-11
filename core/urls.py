5f78bc090f7c2180db16a251d03acdee31a45d59
ahmed007dr <ahmed007eg@gmail.com>
2024-04-11 16:12:44 +0200

    sign up template and View

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
-from .views import frontpage
+from .views import frontpage ,singup
 
 
 urlpatterns = [
     path('',frontpage),
+    path('singup/',singup),
 ]
 
--- core/views.py
+++ core/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login
 
+from django.shortcuts import render,redirect
+
+from .forms import SingUpForm
 # Create your views here.
 
 def frontpage(request):
     return render(request , 'core/frontpage.html')
+
+def singup(request):
+    if request.method == 'POST':
+        form = SingUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('frontpage')
+    else:
+        form = SingUpForm()
+
+    return render (request,'core/singup.html',{'form':form})
