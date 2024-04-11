1edddb9ccfcc5da1c24ff189db3777b4427de4b9
ahmed007dr <ahmed007eg@gmail.com>
2024-04-11 17:16:24 +0200

    create room app & url & html

--- room/models.py
+++ room/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Room(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
-    
+    def __str__(self):
+        return str(self.name)
