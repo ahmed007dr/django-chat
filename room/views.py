ec5fe04274101ffef7d12379cbb17cc2444986a6
ahmed007dr <ahmed007eg@gmail.com>
2024-04-12 04:11:00 +0200

    store messge

--- room/views.py
+++ room/views.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 
-from .models import Room
+from .models import Room ,Message
 
 @login_required
 def rooms(request):
     rooms = Room.objects.all()
     return render(request, 'room/rooms.html', {'rooms': rooms})
 
+
+@login_required
 def room_detail(request, room_slug):
     room = get_object_or_404(Room, slug=room_slug)
-    return render(request, 'room/room_detail.html', {'room': room})
+    messages = Message.objects.filter(room=room)[0:25]
+    return render(request, 'room/room_detail.html', {'room': room , 'messages': messages})
